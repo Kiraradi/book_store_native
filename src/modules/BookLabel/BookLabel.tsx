8779712754261c1eeb9b707e3513d0e8e2ccd8a5
Kiraradi <katel1998@yandex.ru>
2024-11-02 18:18:04 +0300

    fix bookLabel

--- src/modules/BookLabel/BookLabel.tsx
+++ src/modules/BookLabel/BookLabel.tsx
@@ -3,6 +3,7 @@ import {IBook} from '../../interfaces';
 import {Image, Pressable, StyleSheet, View} from 'react-native';
 import {SERVER_URL} from '../../../config';
 import PoppinsText from '../../UI/CustomsTexts/PoppinsText';
+import CustomButton from '../../UI/CustomButton/CustomButton';
 
 const BookLabel: FC<IBook> = props => {
   return (
@@ -15,6 +16,7 @@ const BookLabel: FC<IBook> = props => {
       </Pressable>
       <PoppinsText styles={styles.name}>{props.name}</PoppinsText>
       <PoppinsText styles={styles.author}>{props.author}</PoppinsText>
+      <CustomButton text={`$ ${props.price} USD`} callBack={() => null} />
     </View>
   );
 };
@@ -23,14 +25,14 @@ const styles = StyleSheet.create({
   wrapper: {
     width: '50%',
     paddingVertical: 5,
-    height: 300,
+    height: 400,
     flexDirection: 'column',
     padding: 10,
   },
   coverButton: {
     flex: 1,
   },
-  img: {width: '100%', height: '100%', borderRadius: 16},
+  img: {width: '100%', height: '100%', borderRadius: 16, maxHeight: 350},
   name: {
     fontSize: 18,
   },
